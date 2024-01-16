2794e4d21e718cb00f3c88097765a8c9372eb516
irinaisheree <irinastoyanova@abv.bg>
2024-01-16 13:36:51 +0200

    add cube controller

--- src/config/expressCongfig.js
+++ src/config/expressCongfig.js
@@ -4,7 +4,7 @@ const path = require('path')
 function expressConfig(app){
     //Express config
 app.use(express.static(path.resolve(__dirname, '../public')))
-app.urlencoded({extended : false})
+app.use(express.urlencoded({extended : false}))
 }
 
 module.exports = expressConfig
--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 
 const expressConfig = require('./config/expressCongfig')
-const handlebarsConfig = require('./config/handlebarsConfig')
+const handlebarsConfig = require('./config/handlebarsConfig') 
+
 const homeController = require('./controllers/homeController')
+const cubeController = require('./controllers/cubeController')
 
 const PORT = 5000
 
@@ -11,6 +13,7 @@ expressConfig(app)
 handlebarsConfig(app)
 
 app.use(homeController)
+app.use('/cubes', cubeController)
 
 
 
