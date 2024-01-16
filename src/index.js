fdc296cc4b01f517155751cbdf850bc1b9609c31
irinaisheree <irinastoyanova@abv.bg>
2024-01-16 11:42:01 +0200

    Added express to the project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('Hello world')
+const express = require('express')
+const app = express()
+
+const PORT = 5000
+
+app.get('/', (req,res) =>{
+    res.send('Hello from Express!')
+})
+
+app.listen(PORT, ()=> {console.log(`Server is running on port ${PORT}...`)})
