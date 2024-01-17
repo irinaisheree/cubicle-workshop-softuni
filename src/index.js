1f1bf3b9c0d06af386faddbe60e6233fb975b059
irinaisheree <irinastoyanova@abv.bg>
2024-01-17 13:29:25 +0200

    extract routes

--- src/index.js
+++ src/index.js
@@ -4,19 +4,16 @@ const app = express()
 const expressConfig = require('./config/expressCongfig')
 const handlebarsConfig = require('./config/handlebarsConfig') 
 
-const homeController = require('./controllers/homeController')
-const cubeController = require('./controllers/cubeController')
+const routes = require('./routes')
+
 
 const PORT = 5000
 
 expressConfig(app)
 handlebarsConfig(app)
 
-app.use(homeController)
-app.use('/cubes', cubeController)
-app.get('*', (req,res) => {
-    res.redirect('/404')
-})
+app.use(routes)
+
 
 
 
