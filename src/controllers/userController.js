6b726b210f14caa7427d187a8ec544c3e79059b5
irinaisheree <irinastoyanova@abv.bg>
2024-02-07 15:05:59 +0200

    save user with validated password

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,9 +1,17 @@
 const router = require('express').Router()
 
+const userManager = require('../managers/userManager')
+
 router.get('/register', (req, res) => {
     res.render('users/register')
 })
 
-router.post()
+router.post('/register', async(req, res) => {
+    const {username, password, repeatPassword} = req.body
+
+    await userManager.register({username, password, repeatPassword})
+    
+    res.redirect('/users/login')
+})
 
 module.exports = router;
